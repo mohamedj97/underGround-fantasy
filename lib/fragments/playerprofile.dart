7d66239912d614bcf3ea30bbd2ead6caf57c7743
Gasserashraf <gasser.ashraf977@gmail.com>
2020-07-15 23:02:33 +0200

    profile image is done 11

--- lib/fragments/playerprofile.dart
+++ lib/fragments/playerprofile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 
 
